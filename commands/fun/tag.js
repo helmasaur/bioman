7924b1f3b228c930e1724cb2240339cbc394dff2
Helmasaur <helma1138@gmail.com>
2017-05-07 00:26:45 +0200

    Discord.js update and equalities correction
    - Discord.js updated to version 11.1
    - The equalties with a `=` missing have all been corrected

--- commands/fun/tag.js
+++ commands/fun/tag.js
@@ -7,7 +7,7 @@ module.exports = class KickCommand extends commando.Command {
 			group: 'fun',
 			memberName: 'tag',
 			description: 'Command to send a message depending on a keyword.',
-			guildOnly: true,
+			guildOnly: false,
 
 			args: [{
 				key: 'name',
@@ -18,14 +18,20 @@ module.exports = class KickCommand extends commando.Command {
 		})
 	}
 
-	async run (msg, args) {
+	async run(msg, args) {
 		const keyword = args.name.toLowerCase();
 		const tags = require('../../data/tags.json');
 
+	 	var tag = (tags.find(t => t.keyword === keyword).display);
+
 		for (let i = 0; tags.length > i; i++) {
         	if (tags[i].keyword === keyword) {
-				return msg.channel.sendMessage(tags[i].display);
+				console.log(`The tag "${keyword}" has been sent.`);
+        		return msg.channel.sendMessage(`*tags[i].display*`);
+        	} else {
+				console.log(`*No tag has been found using the keyword: ${keyword}.*`);
+				return msg.reply(`*No tag has been found using the keyword: ${keyword}.*`);
 			}
-		}
+    	}
 	}
 }
