f566848d61be99b4026052b7045033a3e4e9d9d3
Helmasaur <helma1138@gmail.com>
2017-07-10 23:55:45 +0200

    🐛 Activate channel events only if it's a guild channel
    Before, when the bot used DM, a channel event was triggered.

--- events/channel/channelUpdate.js
+++ events/channel/channelUpdate.js
@@ -1,21 +1,22 @@
 const tools = require('../../util/objectHelper.js');
 
 module.exports = (oldChannel, newChannel) => {
+	if (channel.type === 'text' || channel.type === 'voice') {
+		// Channel's name updated
+		if (newChannel.name !== oldChannel.name) {
+			console.log(`The channel ${oldChannel.name} is now called ${newChannel.name}.`);
+			newChannel.send(`*The channel **#${oldChannel.name}** is now called ${newChannel}.*`);
+		}
 
-	// Channel's name updated
-	if (newChannel.name !== oldChannel.name) {
-		console.log(`The channel ${oldChannel.name} is now called ${newChannel.name}.`);
-		newChannel.send(`*The channel **#${oldChannel.name}** is now called ${newChannel}.*`);
-	}
-
-	// Channel's topic updated
-	if (newChannel.topic !== oldChannel.topic) {
-		if(tools.isEmpty(newChannel.topic)) {
-			console.log(`The channel ${newChannel.name}'s topic has been deleted.`);
-			newChannel.send(`*The channel ${newChannel}'s topic has been deleted.*`);
-		} else {
-			console.log(`The channel ${newChannel.name}'s topic has been updated: ${newChannel.topic}`);
-			newChannel.send(`The channel *${newChannel}'s topic has been updated:\n\`\`\`${newChannel.topic}\`\`\`*`);
+		// Channel's topic updated
+		if (newChannel.topic !== oldChannel.topic) {
+			if(tools.isEmpty(newChannel.topic)) {
+				console.log(`The channel ${newChannel.name}'s topic has been deleted.`);
+				newChannel.send(`*The channel ${newChannel}'s topic has been deleted.*`);
+			} else {
+				console.log(`The channel ${newChannel.name}'s topic has been updated: ${newChannel.topic}`);
+				newChannel.send(`The channel *${newChannel}'s topic has been updated:\n\`\`\`${newChannel.topic}\`\`\`*`);
+			}
 		}
 	}
 };
