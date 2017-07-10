72f7b94405afd25c8728a29d809a096c6c32a0a5
Helmasaur <helma1138@gmail.com>
2017-07-10 23:53:42 +0200

    ✨ Description argument for kick/ban commands"
    - A second argument has been added to the kick and command to indicate the reason of the action. It's possible to leave it blank (close #44).
    - Some corrections about the invitation link after a kick has beed made too (#51)

--- commands/admin/ban.js
+++ commands/admin/ban.js
@@ -9,12 +9,29 @@ module.exports = class BanCommand extends commando.Command {
 			description: 'Makes the bot ban someone.',
 			guildOnly: true,
 
+<<<<<<< Updated upstream
 			args: [{
 				key: 'member',
 				label: 'user',
 				prompt: 'Which member do you want to ban?',
 				type: 'member'
 			}]
+=======
+			args: [
+				{
+					key: 'member',
+					label: 'user',
+					prompt: 'Which member do you want to ban?',
+					type: 'member'
+				},
+				{
+					key: 'description',
+					prompt: '',
+					label: 'description',
+					type: 'string'
+				}
+			]
+>>>>>>> Stashed changes
 		});
 	}
 
@@ -23,6 +40,16 @@ module.exports = class BanCommand extends commando.Command {
 		const bot = guild.me;
 		const commander = msg.member;
 		const member = args.member;
+<<<<<<< Updated upstream
+=======
+		let description;
+
+		if (args.description === '') {
+			description = 'no reason given';
+		} else {
+			description = args.description;
+		}
+>>>>>>> Stashed changes
 
 		if (!bot.hasPermission('BAN_MEMBERS')) {
 			console.log(`Bioman couldn\'t the member ${member.user.tag} because he didn't have the permission.`);
@@ -34,10 +61,18 @@ module.exports = class BanCommand extends commando.Command {
 			console.log(`The member ${commander.useruser.tag} tried to ban the member ${commander.user.tag} but he didn't have the permission.`);
 			return msg.reply(`*You don't have the permission to ban the member ${member.user}.*`);
 		} else {
+<<<<<<< Updated upstream
 			//guild.ban(member, { reason: `${commander.id}` });
 			console.log(`The member ${user} has been banned by the member ${commander.user.tag}.`);
 			guild.defaultChannel.send(`*The member ${user} has been banned by ${commander.user}.*`);
 			return member.send(`You have been banned by ${commander.user}.`);
+=======
+			guild.ban(member, {reason: description}});
+
+			console.log(`The member ${user} has been banned by the member ${commander.user.tag} (${description}).`);
+			guild.defaultChannel.send(`*The member ${user} has been banned by ${commander.user} (${description}).*`);
+			return member.send(`You have been banned by ${commander.user} ${description}.`);
+>>>>>>> Stashed changes
 		}
 	}
 };
--- commands/admin/kick.js
+++ commands/admin/kick.js
@@ -9,19 +9,48 @@ module.exports = class KickCommand extends commando.Command {
 			description: 'Makes the bot kick someone.',
 			guildOnly: true,
 
+<<<<<<< Updated upstream
 			args: [{
 				key: 'member',
 				label: 'user',
 				prompt: 'Which member do you want to kick?',
 				type: 'member'
 			}]
+=======
+			args: [
+				{
+					key: 'member',
+					label: 'user',
+					prompt: 'Which member do you want to kick?',
+					type: 'member'
+				},
+				{
+					key: 'description',
+					label: 'description',
+					prompt: '',
+					type: 'string',
+					default : ''
+				}
+			]
+>>>>>>> Stashed changes
 		});
 	}
 
 	async run(msg, args) {
 		const bot = msg.guild.me;
 		const commander = msg.member;
+<<<<<<< Updated upstream
 		let member = args.member;
+=======
+		const member = args.member;
+		let description;
+
+		if (args.description === '') {
+			description = 'no reason given';
+		} else {
+			description = args.description;
+		}
+>>>>>>> Stashed changes
 
 		if (!bot.hasPermission('KICK_MEMBERS')) {
 			console.log(`Bioman couldn\'t kick the member ${member.user.tag} because he doesn't have the permission.`);
@@ -42,20 +71,31 @@ module.exports = class KickCommand extends commando.Command {
 			console.log(`The member ${commander.user.tag} tried to kick ${commander.user.tag} but he doesn't have the permission.`);
 			return msg.reply(`*You don't have the permission to kick ${member.user}.*`);
 		} else {
+<<<<<<< Updated upstream
 			member.kick();
 			const invite = await msg.defaultChannel.createInvite({maxAge: 0, maxUses: 1});
 
 			console.log(`The member ${member.user.tag} has been kicked by the member ${commander.user.tag}.`);
 			msg.channel.send(`*The member has been ${member.user} has been kicked by ${commander.user}.*`);
 			member.send(`You have been kicked by ${commander.user}.`);
+=======
+			console.log(`The member ${member.user.tag} has been kicked by the member ${commander.user.tag} (${description}).`);
+			msg.channel.send(`*The member has been ${member.user} has been kicked by ${commander.user} (${description}).*`);
+			member.send(`You have been kicked by ${commander.user} (${description}).`);
+>>>>>>> Stashed changes
 
 			if (bot.hasPermission('CREATE_INSTANT_INVITE')) {
+				const invite = await msg.guild.defaultChannel.createInvite({maxAge: 0, maxUses: 1});
+
 				console.log(`An invitation link has been send to the kicked member ${member.user.tag}`)
-				return member.send(invite.url);
+				await member.send(invite.url);
 			} else {
 				console.log(`Bioman couldn't invite the kicked member ${member.user.tag} because he doesn't have the permission`);
-				return msg.reply(`*I couldn't send an invitation link to the kicked member ${member.user} because I don't have the permission to create invitations.*`);
+				await msg.reply(`*I couldn't send an invitation link to the kicked member ${member.user} because I don't have the permission to create invitations.*`);
 			}
+
+			member.kick(description});
+			return;
 		}
 	}
 };
