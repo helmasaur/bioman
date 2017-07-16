02b55a1e0e4749fea61d26130a6431dd90abd479
Helmasaur <helma1138@gmail.com>
2017-07-16 21:44:32 +0200

    ⚡️ Bioman doesn't pronounce asterisks
    A message is first sent for the talking (the text is deleted) and a second, with the Markdown format is sent. (fix #59)

--- commands/fun/say.js
+++ commands/fun/say.js
@@ -24,7 +24,10 @@ module.exports = class SayCommand extends commando.Command {
 		if (commander.hasPermissions('SEND_TTS_MESSAGES')) {
 			msg.delete();
 			console.log(`The member ${commander.user.tag} made Bioman says: "${sentence}"`);
-			msg.channel.send(`*${sentence}*`, {tts: true});
+			msg.channel.send(`${sentence}`, {tts: true}).then(message => {
+				message.delete();
+				msg.channel.send(`*${sentence}*`);
+			});
 		} else {
 			console.log(`The member ${commander.user.tag} tried to make Bioman say: "${sentence}".`);
 			return msg.reply('*I don\'t have the right to repeat what you said.*');
