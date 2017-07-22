3b721aab5ccf04f31d917a8d257e533f7264dedc
Helmasaur <helma1138@gmail.com>
2017-07-22 18:22:26 +0200

    🐛 Say command corrections
    - The TTS voice now pronounces "Bioman" the right way (fixed #67)
    - Authorisation checking for the say command (fixed #68)

--- events/member/displayNameUpdate.js
+++ events/member/displayNameUpdate.js
@@ -1,6 +1,9 @@
 module.exports = (oldMember, newMember) => {
 	let guild = newMember.guild;
 
-	console.log(`The member ${oldMember.displayName} is now called ${newMember.displayName}.`);
-	guild.defaultChannel.send(`*The member **${oldMember.displayName}** is now called ${newMember.user}.*`);
+	// Checks if it's not the bot's name which is modified during a "say" command
+	if (oldMember.displayName === 'Bioman' && newMember === 'Biomane' || oldMember.displayName === 'Biomane' && newMember === 'Bioman') {
+		console.log(`The member ${oldMember.displayName} is now called ${newMember.displayName}.`);
+		guild.defaultChannel.send(`*The member **${oldMember.displayName}** is now called ${newMember.user}.*`);
+	}
 };
