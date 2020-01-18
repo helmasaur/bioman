5850d44624cd4452747e657d3e624f83f4f8cd42
Helmasaur <contact@helmasaur.fr>
2020-01-18 17:51:14 +0100

    ✨ Dice command added
    - command `!roll` added (close #9)
    - package added: trpg-dice
    - some forgotten i18n strings removed (#118)

---
+++ commands/games/roll.js
@@ -0,0 +1,66 @@
+const { Command } = require('discord-akairo');
+const Discord = require('discord.js');
+const i18n = require('i18next');
+const config = require('../../config.json');
+const dice = require('trpg-dice');
+
+class RollCommand extends Command {
+	constructor() {
+		super('roll', {
+			aliases: ['dice', 'roll'],
+			category: 'games',
+			args: [
+				{
+					id: 'roll',
+				}
+			]})
+	}
+
+	async exec(msg, args) {
+		const author = msg.member;
+		const roll = args.roll;
+		let result;
+
+		// Prevents huge rolls
+		if (roll.length > 5) {
+			return msg.reply(i18n.t('roll.error.length'));
+		}
+		// Roll is an integer
+		if (parseInt(roll) !== NaN) {
+			return msg.reply(i18n.t('roll.error.value'));
+		}
+
+		dice.roll(roll, function(err, res) { 
+			if (err) {
+				result = null;
+				// throw err;
+			} else {
+				result = res;
+			}
+		});
+
+		if (result === null) {
+			return msg.reply(i18n.t('roll.error.value'));
+		} else {
+			return msg.channel.send({embed: this.embed(author.user, result)});
+		}
+	}
+
+	embed(author, result) {
+		const embed = new Discord.RichEmbed()
+			.setTitle(i18n.t('roll.embed.title'))
+			.setAuthor(author.tag, author.displayAvatarURL)
+			.setColor(config.richEmbedColors.games)
+			.setDescription(result.expression)
+			.addField(i18n.t('roll.embed.sum'), result.rolls[0].result, true)
+			.addField(i18n.t('roll.embed.result'), result.rolls[0].resultString, true)
+			.addBlankField(true)
+			.addField(i18n.t('roll.embed.min'), result.min, true)
+			.addField(i18n.t('roll.embed.max'), result.max, true)
+			.addField(i18n.t('roll.embed.avg'), result.avg, true);
+		
+		return embed;	
+	}
+}
+
+module.exports = RollCommand;
